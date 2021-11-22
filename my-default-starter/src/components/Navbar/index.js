5965f1db5b695dfdd099e34b3baef781015ca026
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-11-22 18:44:20 +0530

    Update index.js

--- my-default-starter/src/components/Navbar/index.js
+++ my-default-starter/src/components/Navbar/index.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { IconContext } from "react-icons/lib"
+import { FaBars, FaTimes } from "react-icons/fa"
 
 const Navbar = () => {
+    const [click, setClick] = useState(false);
+    const [scroll, setScroll] = useState(false);
+
     return (
-        <div>
-            
-        </div>
+        <React.Fragment>
+            <IconContext.Provider value={{ color: "#141414"}}>
+                <Nav active={scroll} click={click}></Nav>
+            </IconContext.Provider>
+        </React.Fragment>
     )
 }
 
