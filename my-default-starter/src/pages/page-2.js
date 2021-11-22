d031a808c1d9951914d5308bc71a46d8588c8c5a
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-11-22 16:49:33 +0530

    Update page-2.js

--- my-default-starter/src/pages/page-2.js
+++ my-default-starter/src/pages/page-2.js
@@ -4,13 +4,10 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const SecondPage = () => (
+const Images = () => (
   <Layout>
-    <Seo title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <h1>Images</h1>
   </Layout>
 )
 
-export default SecondPage
+export default Images
