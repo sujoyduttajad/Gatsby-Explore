d29181c9408fff6aef1a49a9f2bbaf5489d536f9
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2021-11-22 16:52:32 +0530

    Create destinations.js

---
+++ my-default-starter/src/pages/destinations.js
@@ -0,0 +1,10 @@
+import * as React from "react"
+import Layout from "../components/layout"
+
+const Destinations = () => (
+  <Layout>
+    <h1>Images</h1>
+  </Layout>
+)
+
+export default Destinations
